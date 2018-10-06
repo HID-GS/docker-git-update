6e359d4770601818091684421387226695b2398c
Peter Carrero <peter.carrero@gmail.com>
2018-10-06 11:31:22 -0500

    Add initial code to repo

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM centos:7
+RUN yum -y groupinstall "Development Tools" \
+    && yum -y install \
+              gettext-devel \
+              openssl-devel \
+              perl-CPAN \
+              perl-devel \
+              zlib-devel
+
---
+++ compilegit.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import glob
+import json
+import os
+import shutil
+import sys
+import urllib
+from functools import reduce
+from subprocess import Popen, PIPE
+
+try:
+    git_url = 'https://api.github.com/repos/git/git/tags'
+    git_tags = json.loads(urllib.urlopen(git_url).read())
+
+    git_versions = [x['name'].split('.') for x in git_tags if x['name'].find('-rc') == -1]
+
+    git_current = '.'.join(reduce(lambda x, y: x if x[0] >= y[0] else x if x[1] >= y[1] else x if x[2] >= y[2] else y, git_versions))
+
+    git_tar = reduce(lambda x, y: x if x['name'] == git_current else y, git_tags)['tarball_url']
+
+    print 'fetching latest git verstion %s' % git_current
+    urllib.urlretrieve(git_tar, 'git.tar.gz')
+
+    shutil.rmtree('git', ignore_errors=True)
+    os.mkdir('git')
+
+    print 'unpacking git'
+    p = Popen(['tar', '-zvxf', 'git.tar.gz', '-C', 'git'], stdout=PIPE, stderr=PIPE)
+
+    (stdout, stderr) = p.communicate()
+    print stderr
+
+    print 'configuring git'
+    print os.getcwd()
+    print glob.glob('git/*')
+    os.chdir(glob.glob('git/*')[0])
+    print os.getcwd()
+    Popen(['make', 'configure'])
+    Popen(['./configure', '--prefix=/usr/local'])
+
+    print 'compiling git'
+    Popen(['make', '-j8'])
+
+    print 'installing git'
+    Popen(['make', 'install'])
+
+except:
+    print 'Unexpected error:'
+    print sys.exc_info()
